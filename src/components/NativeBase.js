512b2a5162b17a87d448d8d869d41bf74f0022b1
Pedro Mazala <Pedro Mazala@teksystem.local>
2017-07-13 17:35:54 -0300

    Bagunçado mas mostrando os ícones :)

--- src/components/NativeBase.js
+++ src/components/NativeBase.js
@@ -15,19 +15,46 @@ import {
   Spinner,
   List,
   ListItem,
+  View,
   Text,
   Thumbnail
 } from 'native-base';
 
 import getTheme from '../../native-base-theme/components';
 
+import IconTest from 'react-native-vector-icons/Ionicons';
+function getIconTest() {
+  return (<IconTest name="ios-person" size={30} color="#4F8EF7"/>);
+}
+import {FontAwesome} from '@expo/vector-icons';
+import {Image} from 'react-native';
+import {Asset, Font} from 'expo';
+function cacheAssetsAsync({images = [], fonts = []}) {
+  return Promise.all([...cacheImages(images), ...cacheFonts(fonts)]);
+}
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
 export default class extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      search: "",
+      search: "dcomposer",
       lading: false,
+      isReady: false,
       results: {
         items: []
       }
@@ -60,30 +87,67 @@ export default class extends Component {
       });
   }
 
+  componentWillMount() {
+    this.setState({appIsReady: true});
+    this._loadAssetsAsync();
+  }
+
+  async _loadAssetsAsync() {
+    // try {
+    //   await cacheAssetsAsync({
+    //     fonts: [
+    //       FontAwesome.font,
+    //       {'space-mono': require('native-base/Fonts/Roboto_medium.ttf')},
+    //     ],
+    //   });
+    // } catch (e) {
+    //   console.warn(
+    //     'There was an error caching assets (see: main.js), perhaps due to a ' +
+    //     'network timeout, so we skipped caching. Reload the app to try again.'
+    //   );
+    //   console.log(e.message);
+    // } finally {
+    //   this.setState({appIsReady: true});
+    // }
+  }
+
   render() {
+    // if (!this.state.isReady) {
+    //   return <Expo.AppLoading />;
+    // }
+
     return (
       <StyleProvider style={getTheme()}>
         <Container>
           <Content>
             <Header searchBar rounded>
               {/*<InputGroup>*/}
-              <Icon name="ios-search"/>
+              <Icon label="jobron" name="search"/>
+              {/*{getIconTest()}*/}
               <Input placeholder="Search" value={this.state.search}
                      onChangeText={(text) => this.setState({search: text})} onSubmitEditing={() => this.search()}/>
-              {/*</InputGroup>*/}
-              <Button transparent onPress={() => this.search()}>
+              <Button onPress={() => this.search()}>
                 <Text>Go</Text>
               </Button>
+              {/*</InputGroup>*/}
             </Header>
             {
               this.state.loading ?
-                <Spinner /> :
+                <View>
+                  <Spinner />
+                </View> :
                 <List dataArray={this.state.results.items} renderRow={(item) =>
                   <ListItem button onPress={() => this.setModalVisible(true, item)}>
-                    <Thumbnail square size={80} source={{uri: item.owner.avatar_url}}/>
-                    <Text>Name: <Text style={{fontWeight: '600', color: '#46ee4b'}}>{item.name}</Text></Text>
-                    <Text style={{color: '#007594'}}>{item.full_name}</Text>
-                    <Text note>Score: <Text note style={{marginTop: 5}}>{item.score}</Text></Text>
+                    <Thumbnail circle size={80} source={{uri: item.owner.avatar_url}}/>
+                    <Text style={{padding: 10}}>
+                      <Text>Name:
+                        <Text style={{fontWeight: '600', backgroundColor: "yellow"}}> {item.name}</Text>
+                      </Text>
+                      <Text style={{color: '#007594'}}>{"\n"}{item.full_name}</Text>
+                      <Text note>{"\n"}Score:
+                        <Text note style={{marginTop: 5}}>{item.score}</Text>
+                      </Text>
+                    </Text>
                   </ListItem>
                 }/>
             }
