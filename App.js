24694d32476acd8f81e08d79e02433ec272616fc
Pedro Mazala <Pedro Mazala@teksystem.local>
2017-07-19 09:43:23 -0300

    Modal test

--- App.js
+++ App.js
@@ -3,5 +3,6 @@ import React from 'react';
 import jobron from './src/components/HelloWorld';
 import nb from './src/components/NativeBase';
 import h2 from './src/components/Header2';
+import m from './src/components/Modal';
 
 export default nb;
---
+++ src/components/Modal.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Pedro Mazala on 18/07/2017.
+ */
+import React, { Component } from 'react';
+import { Modal, Text, TouchableHighlight, View } from 'react-native';
+
+export default class extends Component {
+
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+        >
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight onPress={() => {
+                this.setModalVisible(!this.state.modalVisible)
+              }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight onPress={() => {
+          this.setModalVisible(true)
+        }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+
+      </View>
+    );
+  }
+}
